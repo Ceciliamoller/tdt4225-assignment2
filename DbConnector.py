c479ce77cc72857d3e2a447026dee19dd6afa706
JustBroch <just1998@live.no>
2025-10-06 13:48:23 +0200

    add docker-compose.yml and change gitignore

--- DbConnector.py
+++ DbConnector.py
@@ -19,14 +19,18 @@ class DbConnector:
     PASSWORD = "test123" // The password you set for said user
     """
 
-    def __init__(self,
-                 HOST=os.getenv("DB_HOST", "host"),
-                 DATABASE=os.getenv("DB_DATABASE", "assignment_2"),
-                 USER=os.getenv("DB_USER", "student"),
-                 PASSWORD=os.getenv("DB_PASSWORD", "password")):
+    def __init__(
+        self,
+        HOST=os.getenv("DB_HOST", "host"),
+        DATABASE=os.getenv("DB_DATABASE", "assignment_2"),
+        USER=os.getenv("DB_USER", "student"),
+        PASSWORD=os.getenv("DB_PASSWORD", "password"),
+    ):
         # Connect to the database
         try:
-            self.db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=3306)
+            self.db_connection = mysql.connect(
+                host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=3306
+            )
         except Exception as e:
             print("ERROR: Failed to connect to db:", e)
 
